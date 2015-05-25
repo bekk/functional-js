13136ec058eedaa22f0c041b60d67a639917f27b
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-05-25 23:22:35 +0200

    Move part1 from tape to mocha

--- _workshop/01-intro.js
+++ _workshop/01-intro.js
@@ -21,186 +21,141 @@ slides: http://omniscientjs.github.io/workshop-slides/#9
 // basics of JavaScript, to ensure that you fully understand how
 // functions work in JavaScript.
 
-test('first things first', function(t) {
-    // In these tests we rely on `tape`, which is a small testing
-    // library. Throughout these tests you will see the assertions as
-    // follows:
+function hello() {
+  return this + ' says hello';
+}
 
-    // If we are testing a primitive (number, string, boolean) we'll
-    // use `equal`:
+function helloTo(thing) {
+  return this + " says hello " + thing;
+}
 
-    t.equal("test", "test");
+describe('this, call, apply', function() {
+  // In JavaScript we can dynamically choose the value of `this`
+  // using several techniques. Today we will focus on some of these,
+  // starting with `call` and `apply`.
 
-    // This is the same as "test" === "test", i.e. reference equality
+  it('undefined this', function() {
+    var result = null
+    expect(result).to.equal('undefined says hello');
+  });
 
-    // If we are testing objects or arrays, we'll use `deepEqual`:
+  // `call` calls a function with a given `this` value as the first
+  // parameter, then arguments provided individually:
+  //
+  //     func.call(this, arg1, arg2, ...)
 
-    t.deepEqual([1,2,3], [1,2,3]);
+  it('this with call', function() {
+    var result = null;
+    expect(result).to.equal('kim says hello');
+  });
 
-    // If we want to just check that something is `true`, we use `ok`:
+  // `apply` calls a function with a given `this` value as the first
+  // parameter, then arguments provided as an array
+  //
+  //     func.apply(this, [arg1, arg2, ...])
 
-    t.ok(true);
+  it('this with apply', function() {
+    var result = null;
+    expect(result).to.equal('kim says hello');
+  });
 
-    // And lastly, if we expect something to throw an error, we
-    // use `throws`:
+  // If you want to learn more about function invocation in
+  // JavaScript, I recommend reading:
+  // http://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
 
-    t.throws(function() {
-        throw new Error("wtf?");
-    });
+  // By the way, JavaScript is not strict about the number of
+  // arguments, so you can for example include more parameters than
+  // the function expects:
 
-    // Just telling tape we're done
-    t.end();
+  it('javascript fault tolerance', function() {
+    var result = helloTo.call("kim", "world", "or", "maybe", "bekk");
+    expect(result).to.equal('kim says hello world');
+  });
+
 });
 
-test('this, call, apply', function (t) {
-    // In JavaScript we can dynamically choose the value of `this`
-    // using several techniques. Today we will focus on some of these,
-    // starting with `call` and `apply`.
+function argCount() {
+    return arguments.length;
+}
 
-    // `call` calls a function with a given `this` value as the first
-    // parameter, then arguments provided individually:
-    //
-    //     func.call(this, arg1, arg2, ...)
+describe('arguments', function() {
+  // Next up when understanding functions in JavaScript, is
+  // understanding arguments.
 
-    // `apply` calls a function with a given `this` value as the first
-    // parameter, then arguments provided as an array
-    //
-    //     func.apply(this, [arg1, arg2, ...])
+  // Inside a function you will always have access to `arguments`,
+  // which is an array-like object that contains all the arguments
+  // passed to the function.
 
-    function hello() {
-        return this + " says hello";
-    }
+  it('count arguments', function() {
+    var result = argCount(1, 2, 3);
+    expect(result).to.equal(3);
+  });
 
-    t.equal(
-        hello(),
-        "undefined says hello"
-    );
+  it('count arguments', function() {
+    var result = argCount(null, 2, 3);
+    expect(result).to.equal(2);
+  });
 
-    // PROBLEM: Invoke `hello` using `call` to get this test to run
-    t.equal(
-        null,
-        "kim says hello"
-    );
+  // PROBLEM: Multiply `this` and the first argument, without
+  // specifying any parameters
 
-    // PROBLEM: Invoke `hello` using `apply` to get this test to run
-    t.equal(
-        null,
-        "kim says hello"
-    );
+  function multiplyFirst() {
+  }
 
-    function helloTo(thing) {
-        return this + " says hello " + thing;
-    }
+  it('multiplyFirst 1*1', function() {
+    var result = multiplyFirst(1,1);
+    expect(result).to.equal(1);
+  });
 
-    // PROBLEM: Invoke `helloTo` using `call` to get this test to run
-    t.equal(
-        null,
-        "kim says hello world"
-    );
+  it('multiplyFirst 2*5', function() {
+    var result = multiplyFirst(2,5);
+    expect(result).to.equal(10);
+  });
 
-    // PROBLEM: Invoke `helloTo` using `apply` to get this test to run
-    t.equal(
-        null,
-        "kim says hello world"
-    );
-
-    // If you want to learn more about function invocation in
-    // JavaScript, I recommend reading:
-    // http://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
-
-    // By the way, JavaScript is not strict about the number of
-    // arguments, so you can for example include more parameters than
-    // the function expects:
-
-    t.equal(
-        helloTo.call("kim", "world", "or", "maybe", "bekk"),
-        "kim says hello world"
-    );
-
-    t.end();
 });
 
-test('arguments', function(t) {
-    // Next up when understanding functions in JavaScript, is
-    // understanding arguments.
+describe('prototype & this', function() {
+  // There will not be much about prototypes in this workshop, but
+  // there is one thing it's important to understand, especially when
+  // working with `arguments`: In JavaScript you can borrow existing
+  // functions that live on a prototype.
 
-    // Inside a function you will always have access to `arguments`,
-    // which is an array-like object that contains all the arguments
-    // passed to the function.
+  // Let's look at an example
 
-    function argCount() {
-        return arguments.length;
-    }
+  var User = function(age) {
+      this.age = age;
+  };
 
-    t.equal(
-        argCount(1, 2, 3),
-        3
-    );
+  User.prototype.getAge = function() {
+      return this.age;
+  };
 
-    t.equal(
-        argCount.call(null, 2, 3),
-        2
-    );
-
-    // PROBLEM: Multiply `this` and the first argument, without
-    // specifying any parameters
-
-    function multiplyFirst() {
-    }
-
-    t.equal(
-        multiplyFirst.call(1, 1),
-        1
-    );
-
-    t.equal(
-        multiplyFirst.call(2, 5),
-        10
-    );
-
-    t.end();
-});
-
-test('prototype & this', function(t) {
-    // There will not be much about prototypes in this workshop, but
-    // there is one thing it's important to understand, especially when
-    // working with `arguments`: In JavaScript you can borrow existing
-    // functions that live on a prototype.
-
-    // Let's look at an example
-
-    var User = function(age) {
-        this.age = age;
-    };
-    User.prototype.getAge = function() {
-        return this.age;
-    };
-
-    // Usually, to get the age of a user, we would:
-
+  it('user getAge', function() {
     var user = new User(25);
-    t.equal(user.getAge(), 25);
+    expect(user.getAge()).to.equal(25);
+  });
 
-    // However, in JavaScript we can actually invoke `getAge` without
-    // creating an instance of `User`
+  // However, in JavaScript we can actually invoke `getAge` without
+  // creating an instance of `User`
 
+  it('context free getAge', function() {
     var age = User.prototype.getAge.call({ age: 35 });
-    t.equal(age, 35);
+    expect(age).to.equal(35);
+  });
 
-    // Basically, we borrow a function from `User`. This is a trick we
-    // can use to make `arguments` into an array.
+  // Basically, we borrow a function from `User`. This is a trick we
+  // can use to make `arguments` into an array.
 
-    // PROBLEM: Create a function that "borrows"
-    // `Array.prototype.slice` and returns all the arguments given,
-    // except the first
+  // PROBLEM: Create a function that "borrows"
+  // `Array.prototype.slice` and returns all the arguments given,
+  // except the first
 
-    function rest() {
-    }
+  function rest() {
+  }
 
-    t.deepEqual(
-        rest(1, 2, 3),
-        [2, 3]
-    );
+  it('rest', function() {
+    var result = rest(1, 2, 3);
+    expect(result).to.equal([2, 3]);
+  });
 
-    t.end();
 });
