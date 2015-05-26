e79294d4657a271f14755522b4b604b25acdf980
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-05-26 18:16:27 +0200

    Cleaned part3-1

--- _workshop/03-1-factories.js
+++ _workshop/03-1-factories.js
@@ -8,66 +8,61 @@ name: 03-1-factories
 next: 03-2-higher-order
 slides:
 info: |
-
+  Okey, it is time to take a break from map, filter and reduce, and
+  start focusing more on higher-order functions and what we can do
+  with JavaScript.
 ---
 
-// Okey, it's time to take a break from map, filter and reduce, and
-// start focusing more on higher-order functions and what we can do
-// with JavaScript.
+// In JavaScript we love factories. Let's create a factory that
+// creates adder functions:
 
-describe('function factories!', function() {
-    // In JavaScript we love factories. Let's create a factory that
-    // creates adder functions:
+function makeAdder(arg) {
+  return function(arg2) {
+    return arg + arg2;
+  }
+}
 
-    function makeAdder(arg) {
-        return function(arg2) {
-            return arg + arg2;
-        }
-    }
-
+describe('makeAdder', function() {
     var addTwo = makeAdder(2)
 
-    it('a test', function() {
-			var result =         addTwo(40);
+    it('addTwo should add 2', function() {
+			var result = addTwo(40);
 			expect(result).to.equal(42);
-		});;
-
-    it('a test', function() {
-			var result =         addTwo(3);
-			expect(result).to.equal(5);
-		});;
-
-    // Seems simple enough, but it's a powerful technique that we will
-    // be having a lot of fun with.
-
-    // Okey, it's your turn to create a factory.
-
-    // PROBLEM: Create a counter that returns a function that increases a
-    // number every time it's called.
-
-    function counter() {
-    }
-
-    var c1 = counter();
-
-    it('a test', function() {
-			var result = c1();
-			expect(result).to.equal(0);
 		});
-    it('a test', function() {
-			var result = c1();
-			expect(result).to.equal(1);
-		});
-    it('a test', function() {
-			var result = c1();
-			expect(result).to.equal(2);
-		});
+});
 
-    var c2 = counter();
+// Seems simple enough, but it's a powerful technique that we will
+// be having a lot of fun with.
 
-    it('a test', function() {
-			var result = c2();
-			expect(result).to.equal(0);
-		});
+// Okey, it's your turn to create a factory.
 
+// PROBLEM: Create a counter that returns a function that increases a
+// number every time it's called.
+
+function counter() {
+}
+
+describe('counter', function() {
+  var c1 = counter();
+  var c2 = counter();
+
+  it('should increase by one with each invocation', function() {
+    var result = c1();
+    expect(result).to.equal(0);
+  });
+
+  it('count = 1', function() {
+    var result = c1();
+    expect(result).to.equal(1);
+  });
+
+  it('count = 2', function() {
+    var result = c1();
+    expect(result).to.equal(2);
+  });
+
+  it('should not share state between counters', function() {
+    var result = c2();
+    expect(result).to.equal(0);
+  });
 });
