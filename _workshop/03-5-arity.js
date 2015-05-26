aeb7bafcf3f692ba40619989f0e4f3318b5606e3
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-05-26 19:05:14 +0200

    Cleaned part 3-5

--- _workshop/03-5-arity.js
+++ _workshop/03-5-arity.js
@@ -8,73 +8,77 @@ name: 03-5-arity
 next: 04-1-partial-application
 slides:
 info: |
+  The arity of a function is the number of arguments it takes.
 
+  //TODO: Skriv mer her.
 ---
-describe('understanding arity', function() {
-    // Going from string to int we can use parseInt, e.g.
+describe('arity of parseInt', function() {
+  // Going from string to int we can use parseInt, e.g.
 
-    it('a test', function() {
-			var result = parseInt('123');
-			expect(result).to.equal(123);
-		});
+  it('normal usage', function() {
+		var result = parseInt('123');
+		expect(result).to.equal(123);
+	});
 
-    // However, trying to use this function together with map,
-    // we see a problem:
+  // However, trying to use this function together with map,
+  // we see a problem:
 
-    var parsed = ['1','2','3'].map(parseInt)
-    //=> [1, NaN, NaN]
+  var parsed = ['1','2','3'].map(parseInt)
 
-    it('a test', function() {
-			var result = parsed[0];
-			expect(result).to.equal(1);
-		});
-    it('a test', function() {
-			var result = isNaN(parsed[1]);
-			expect(result).to.ok;
-		});
-    it('a test', function() {
-			var result = isNaN(parsed[2]);
-			expect(result).to.ok;
-		});
+  it('with map first element', function() {
+		var result = parsed[0];
+		expect(result).to.equal(1);
+	});
+
+  it('with map first element', function() {
+		var result = isNaN(parsed[1]);
+		expect(result).to.be.ok;
+	});
+
+  it('with map first element', function() {
+		var result = isNaN(parsed[2]);
+		expect(result).to.be.ok;
+	});
+
     // (we can't use deepEqual since NaN !== NaN in JavaScript)
+});
 
-    // The problem is that map calls each function with three parameters -- the
-    // value, the index, and the array. Instead of the above, we could write:
+describe('fixed parseInt', function() {
+  // The problem is that map calls each function with three parameters -- the
+  // value, the index, and the array. Instead of the above, we could write:
 
-    var parsed2 = ['1','2','3'].map(function(value, index, arr) {
-        return parseInt(value);
-    })
+  var parsed = ['1','2','3'].map(function(value, index, arr) {
+      return parseInt(value);
+  })
 
-    it('a test', function() {
-			var result =         parsed2;
-			expect(result).to.deep.equal([1,2,3]);
-		});
+  it('should work with map', function() {
+		var result = parsed;
+		expect(result).to.deep.equal([1,2,3]);
+	});
 
-    // However, there are a couple of other solutions. We can for example
-    // create a helper to do the job for us, and that lets us write:
-
-    var parsed3 = ['1', '2', '3'].map(unary(parseInt))
-
-    function unary(fn) {
-        return function(arg) {
-            return fn.call(this, arg)
-        }
+  // However, there are a couple of other solutions. We can for example
+  // create a helper to do the job for us, and that lets us write:
+  function unary(fn) {
+    return function(arg) {
+      return fn.call(this, arg)
     }
+  }
 
-    // Here we have created unary, which ensures that a function is only ever
-    // called with one argument, no matter how many you actually send to it.
+  var parsed2 = ['1', '2', '3'].map(unary(parseInt))
 
-    it('a test', function() {
-			var result =         parsed3;
-			expect(result).to.deep.equal([1,2,3]);
-		});
+  // Here we have created unary, which ensures that a function is only ever
+  // called with one argument, no matter how many you actually send to it.
 
-    // However, unary might be to simple in this case, as we actually
-    // might want to call:
-    //
-    //     parseInt(val, 10);
-    //
-    // To get there, however, we should first learn some things about
-    // partial application.
+  it('should work with unary decorator', function() {
+		var result = parsed2;
+		expect(result).to.deep.equal([1,2,3]);
+	});
 
+  // However, unary might be to simple in this case, as we actually
+  // might want to call:
+  //
+  //     parseInt(val, 10);
+  //
+  // To get there, however, we should first learn some things about
+  // partial application.
 });
