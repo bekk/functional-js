76c9d651bcaa1f0324068ec78c03caf2d118b5e6
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-05-26 18:25:48 +0200

    Cleaned part 3-3

--- _workshop/03-3-functions-with-functions.js
+++ _workshop/03-3-functions-with-functions.js
@@ -8,55 +8,56 @@ name: 03-3-functions-with-functions
 next: 03-4-fluent
 slides:
 info: |
+  In JavaScript you can actually add functions onto existing
+  functions. This can create sweet APIs. Let us look at an example.
 
+  I have seen this pattern in many code bases:
+
+  ```
+  if (debug) console.log('some text');
+  ```
+
+  When you want to show debug information you set debug to `true`. You of
+  course need to always remember to write that `if` everywhere in your
+  code. Let us create a create a better API.
 ---
-describe('add functions to functions', function() {
-    // In JavaScript you can actually add functions onto existing
-    // functions. This can create sweet APIs. Let's look at an example.
+// PROBLEM: We want to guard a function, so that it only calls
+// through to the function we pass in once we have called `start`
+// on the guard. (See below for the tests to understand how this is
+// supposed to work.)
 
-    // I've seen this pattern in many code bases:
-    //
-    //     if (debug) console.log('some text');
-    //
-    // When you want to show debug information you set debug to `true`. You of
-    // course need to always remember to write that `if` everywhere in your
-    // code. Let's create a create a better API.
-
-    // PROBLEM: We want to guard a function, so that it only calls
-    // through to the function we pass in once we have called `start`
-    // on the guard. (See below for the tests to understand how this is
-    // supposed to work.)
-
-    function guard(fn) {
-        var guard = function() {
-        };
-
-        // in JavaScript you can actually add
-        // functions to existing functions
-        guard.start = function() {
-        };
-
-        return guard;
+function guard(fn) {
+    var guard = function() {
     };
 
+    // in JavaScript you can actually add
+    // functions to existing functions
+    guard.start = function() {
+    };
+
+    return guard;
+};
+
+describe('guard', function() {
     // Let's now guard a debug helper. To make it a little bit simpler
     // to test, we return a message instead of calling console.log or similar.
     var debug = guard(function(msg) {
         return msg;
     });
 
-    it('a test', function() {
+    it('should not return anything if not started', function() {
 			var result = debug('test 1');
 			expect(result).to.equal(undefined);
 		});
-    it('a test', function() {
+
+    it('should not return anything if not started even when invoked multiple times', function() {
 			var result = debug('test 2');
 			expect(result).to.equal(undefined);
 		});
 
     debug.start();
 
-    it('a test', function() {
+    it('should return if started', function() {
 			var result = debug('test 3');
 			expect(result).to.equal('test 3');
 		});
